b5ee91b4875b81429896be3e20971967856e909c
Wígny Almeida <wignybora@gmail.com>
2020-03-21 20:28:32 -0400

    Correção path Travis

--- ci/script.sh
+++ ci/script.sh
@@ -7,8 +7,6 @@ psql -c "alter user dart with password 'dart';" -U postgres
 psql -c 'create database dart_test;' -U postgres
 psql -c 'grant all on database dart_test to dart;' -U postgres
 
-cd "$TEST_DIR"
-
 pub get
 
 pub run test -j 1 -r expanded $RUNNER_ARGS
@@ -17,5 +15,3 @@ pub run test -j 1 -r expanded $RUNNER_ARGS
 #  pub global activate -sgit https://github.com/stablekernel/aqueduct-coverage-tool.git
 #  pub global run aqueduct_coverage_tool:main
 #fi
-
-cd ..
